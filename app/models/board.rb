fca74ca7144f88be118a5c7157f8c94c52fc1c6c
Dave Goerlich <dgoerlich@designinginteractive.com>
2013-10-06 17:08:33 -0400

    Accessible defined through row 6

--- app/models/board.rb
+++ app/models/board.rb
@@ -6,12 +6,10 @@ class Board
     setup_tiles
   end
 
-private
-
   def setup_tiles
     @tiles = Hash.new
 
-    for i in 0..27 
+    for i in 0..35
       setup_tile_row i
     end
   end
@@ -19,13 +17,52 @@ private
   def setup_tile_row row
     @tiles[row] = Hash.new
 
-    for i in 0..35
+    for i in 0..27
       @tiles[row][i] = Tile.new row, i, compute_accessible_for(row, i)
     end
   end
 
   def compute_accessible_for x, y
+    method = "row_#{x}_accessible"
+
+    if self.respond_to? method
+      self.send(method.to_sym, y)
+    else
+      false
+    end
+
+  end
+
+  def row_0_accessible y
     false
+  end
+
+  def row_1_accessible y
+    false
+  end
+
+  def row_2_accessible y
+    false
+  end
+
+  def row_3_accessible y
+    if y == 0 || y == 27
+      false
+    else
+      true
+    end
+  end
+
+  def row_4_accessible y
+    [1,12,15,27].include? y
+  end
+
+  def row_5_accessible y
+    [1,12,15,27].include? y
+  end
+
+  def row_6_accessible y
+    ![0, 7, 8, 13, 14, 19, 20, 27].include? y
   end
 
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -12,8 +12,8 @@ describe Board do
   context "Setup" do
 
     it "Initializes the board with tiles" do
-      @board.tiles.count.should == 28
-      @board.tiles[0].count.should == 36
+      @board.tiles.count.should == 36
+      @board.tiles[0].count.should == 28
 
       tile = tile_at 4, 26
       tile.x.should == 4
@@ -42,13 +42,49 @@ describe Board do
       it "Sets up Row 3" do
         tile_at(3, 0).accessible?.should be_false
 
-        for i in 1..34
+        for i in 1..26
           tile_at(3, i).accessible?.should be_true
         end
 
-        tile_at(3, 35).accessible?.should be_false
+        tile_at(3, 27).accessible?.should be_false
+      end
+
+      it "Sets up Row 4" do
+        open = [1, 12, 15, 27]
+
+        for i in 0..27
+          if open.include? i
+            tile_at(4, i).accessible?.should be_true
+          else
+            tile_at(4, i).accessible?.should be_false
+          end
+        end
+      end
+
+      it "Sets up Row 5" do
+        open = [1, 12, 15, 27]
+
+        for i in 0..27
+          if open.include? i
+            tile_at(5, i).accessible?.should be_true
+          else
+            tile_at(5, i).accessible?.should be_false
+          end
+        end
+      end
+
+      it "Sets up Row 6" do
+        closed = [0, 7, 8, 13, 14, 19, 20, 27]
+
+        for i in 0..27
+          if closed.include? i
+            tile_at(6, i).accessible?.should be_false
+          else
+            tile_at(6, i).accessible?.should be_true
+          end
+        end
+
       end
     end
   end
-
 end
