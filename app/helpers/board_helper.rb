f9b8949a297fc8152522c5136eddec199e27f09e
Dave Goerlich <dgoerlich@designinginteractive.com>
2013-10-16 14:33:23 -0400

    Add some classes to the board

--- app/helpers/board_helper.rb
+++ app/helpers/board_helper.rb
@@ -1,7 +1,6 @@
 module BoardHelper
 
   def display_tile_at row, column
-
     tile = @board.tile_at row, column
 
     if tile.accessible?
@@ -12,6 +11,20 @@ module BoardHelper
       end
     else
       'X'
+    end
+  end
+
+  def class_for_tile_at row, column
+    tile = @board.tile_at row, column
+
+    if tile.accessible?
+      if @board.pacman.location == tile
+        'pacman'
+      else
+        'available'
+      end
+    else
+      'unavailable'
     end
 
   end
