8f96eaba59bdc1e4bf963bbde21e7bdf0c5ac0f5
Dave Goerlich <dgoerlich@designinginteractive.com>
2013-10-24 18:45:38 -0400

    Controller work to handle the displays we'll need

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -2,6 +2,39 @@ class BoardController < ApplicationController
 
   def show
     @board = Board.new
+
+    if params[:pacman]
+      @show_pacman = true
+
+      movement = params[:pacman].split(",")
+
+      row = movement[0].to_i
+      column = movement[1].to_i
+
+      direction = case  movement[2]
+                  when "n" then :north
+                  when "s" then :south
+                  when "e" then :east
+                  when "w" then :west
+                  else nil
+                  end
+
+      tile = @board.tile_at row, column
+      @board.pacman.move_to tile, direction
+    end
+
+    if params[:show_pacman] == "t"
+      @show_pacman = true
+    end
+
+    if params[:highlight] == "intersection"
+      @highlight_intersection = true
+    end
+
+    if params[:highlight] == "special"
+      @highlight_special = true
+    end
+
   end
 
 end
--- app/helpers/board_helper.rb
+++ app/helpers/board_helper.rb
@@ -3,29 +3,53 @@ module BoardHelper
   def display_tile_at row, column
     tile = @board.tile_at row, column
 
-    if tile.accessible?
-      if @board.pacman.location == tile
-        'P'
-      else
-        ' '
-      end
-    else
-      'X'
+    content = "&nbsp;"
+
+    if @show_pacman && @board.pacman.location == tile
+      content = case  @board.pacman.direction
+                when :north then "&uarr;"
+                when :south then "&darr;"
+                when :east then "&rarr;"
+                when :west then "&larr;"
+                else "&mdash;"
+                end
     end
+
+    content.html_safe
   end
 
   def class_for_tile_at row, column
     tile = @board.tile_at row, column
 
+    css_class = "unavailable"
+
     if tile.accessible?
-      if @board.pacman.location == tile
-        'pacman'
-      else
-        'available'
+      css_class = "available"
+
+      if @show_pacman && @board.pacman.location == tile
+        css_class << ' pacman'
       end
-    else
-      'unavailable'
+
+      if @highlight_intersection && tile.intersection?
+        css_class << ' highlight'
+      end
+
+      if @highlight_special
+        special_tiles = [
+          @board.tile_at(9, 12),
+          @board.tile_at(9, 15),
+          @board.tile_at(21, 12),
+          @board.tile_at(21, 15)
+        ]
+
+        if special_tiles.include? tile
+          css_class << ' highlight'
+        end
+      end
+
     end
+
+    css_class
 
   end
 
